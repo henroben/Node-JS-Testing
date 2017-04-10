8dde46761e8100404e1fc2ec3e2e4124ffa7744f
Ben Mercer <ben@henroben.co.uk>
2017-04-10 10:22:52 +0100

    Expect assertion library added

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,19 +1,51 @@
 "use strict";
 
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('should add two numbers', () => {
     let res = utils.add(33, 11);
 
-    if(res != 44) {
-        throw new Error(`Expected 44, but got ${res}`);
-    }
+    expect(res).toBe(44).toBeA('number');
+
+    // if(res != 44) {
+    //     throw new Error(`Expected 44, but got ${res}`);
+    // }
 });
 
 it('should return the square of supplied number', () => {
     let res = utils.square(3);
 
-    if(res != 9) {
-        throw new Error(`Expected 9, but got ${res}`);
-    }
+    expect(res).toBe(9).toBeA('number');
+
+    // if(res != 9) {
+    //     throw new Error(`Expected 9, but got ${res}`);
+    // }
 });
+
+it('should verify first and last names are set', () => {
+    let user = {
+        age: 43
+    };
+
+    let res = utils.setName(user, "Ben Mercer");
+
+    expect(user).toBeA('object').toInclude({
+        firstName: 'Ben',
+        lastName: 'Mercer'
+    });
+});
+
+// it('should expect some values', () => {
+//     // expect(12).toNotBe(11);
+//     // expect({name: 'Ben'}).toEqual({name: 'Ben'});
+//     // expect([2,3,4]).toInclude(2).toExclude(1);
+//     expect({
+//         name: 'Ben',
+//         age: '43',
+//         location: 'Ipswich'
+//     }).toExclude({
+//         age: 25
+//     });
+// });
